84b55d9450f66cd81231c8f26d3beb43ae2a93c9
KAYA FURKAN İPEKCİ <kfurkan60@hotmail.com>
2026-07-09 00:12:27 +0300

    Wire up React Router with redirect to Airports page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AirportsPage from "./pages/AirportsPage";
 
 function App() {
-  return <AirportsPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/airports" replace />} />
+        <Route path="/airports" element={<AirportsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
