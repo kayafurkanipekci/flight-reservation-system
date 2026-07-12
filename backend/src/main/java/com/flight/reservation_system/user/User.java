19fa0c8a4ca398c1ab432ffb3736892b0f5288a0
KAYA FURKAN İPEKCİ <kfurkan60@hotmail.com>
2026-07-12 21:41:37 +0300

    Add minimal User entity and repository (auth logic deferred to week 2)

---
+++ backend/src/main/java/com/flight/reservation_system/user/User.java
@@ -0,0 +1,31 @@
+package com.flight.reservation_system.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String role;
+}
